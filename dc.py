284c141f45e7637752cca1ac1c1ce5ba911e46ff
naeeon <neheeone@gmail.com>
2024-06-12 09:31:33 +0000

    a

--- dc.py
+++ dc.py
@@ -4,6 +4,7 @@ import random
 import asyncio
 from faker import Faker
 import time
+import string
 
 gel = input("id : ")
 minor = input("is minor? : ")
@@ -14,18 +15,30 @@ i=0
 res = requests.get("https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt")
 txt = res.text.split("\n")
 fake = Faker('ko_KR')
+sendtxt = "요한슈미츠는가해자"
 random.shuffle(txt)
 
 while i < len(txt):
  print(str(i)+"/"+str(len(txt)))
  try:
   async def run():
+   rand_str = ""
+   a = ""
+   j=0
+   for k in range(len(sendtxt)):
+    rand_str += str(random.choice(string.ascii_uppercase + string.digits))
+
+   lst = list(rand_str)
+   finaltxt = list(sendtxt)
+   while j < len(lst):
+    a += finaltxt[j] + lst[j]
+    j=j+1
+   print(a)
    name = fake.name()
    password  = fake.password()
-   title = fake.company()+fake.job()
+   title = a
    content = fake.company()+fake.job()
    proxy = "http://"+txt[i]
-   time.sleep(random.randrange(1,6))
    api = dc_api.API()
    doc_id = await api.write_document(board_id=gel, title=title, contents=content, name=name, password=password,pr=proxy,is_minor=minor)
    await api.close()
